355fe8b3d807d42ce137ef516a53b09a5c971506
John Sasser <cw.john.sasser@gmail.com>
2020-01-07 23:29:47 -0500

    basic html, styling and moment.js

---
+++ assets/javascript/script.js
@@ -0,0 +1,12 @@
+// pseudo code for day planner
+// get all html elements in var's for buttons
+
+// button functions
+
+//  - saves text area to local storage
+//  - change the button to say saved after clicked
+
+// change the color of the <li> based on the time of day
+
+$("#currentTime").text(moment().format("MMMM Do YYYY"));
+console.log(moment());
