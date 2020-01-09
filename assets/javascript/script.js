8685d8eb6936a2a61e369ad306c225a8260beb14
John Sasser <cw.john.sasser@gmail.com>
2020-01-09 14:32:35 -0500

    working version of local storage, splice() out overwritten notes and returns values on window.load();

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,12 +1,57 @@
-// pseudo code for day planner
-// get all html elements in var's for buttons
-
-// button functions
-
-//  - saves text area to local storage
-//  - change the button to say saved after clicked
-
 // change the color of the <li> based on the time of day
 
-$("#currentTime").text(moment().format("MMMM Do YYYY"));
-console.log(moment());
+let currentTime = moment().format("MMM Do YYYY");
+let noteArr = [];
+let $textArea = $(".textInput");
+let $buttons = $(".btn");
+
+function sendNotes() {
+	var inputArr = JSON.stringify(noteArr);
+	localStorage.setItem("userNote", inputArr);
+}
+
+function returnNotes() {
+	var getInput = localStorage.getItem("userNote");
+	var gotNote = JSON.parse(getInput);
+
+	$textArea.each(function(idx) {
+		const textareaId = $(this).attr("id");
+		console.log(idx);
+		for (let i = 0; i < gotNote.length; i++) {
+			if (textareaId == gotNote[i].inputId) {
+				$(this).val(gotNote[i].userInput);
+				// $($textArea[idx]).val(gotNote[i].userInput);
+			}
+		}
+	});
+}
+
+$buttons.on("click", function() {
+	// remove dupe note
+	// loop over notesArr
+	// - check if current obj inputId == this.siblings('.textInput).attr('id)
+	// - if it matched, remove 1st obj from array
+	for (let i = 0; i < noteArr.length; i++) {
+		let textareaId = $(this)
+			.siblings(".textInput")
+			.attr("id");
+		console.log(textareaId, noteArr[i].inputId);
+		if (textareaId == noteArr[i].inputId) {
+			noteArr.splice(i, 1);
+		}
+	}
+	// add new note
+	noteArr.push({
+		userInput: $(this)
+			.siblings(".textInput")
+			.val(),
+		inputId: $(this)
+			.siblings(".textInput")
+			.attr("id")
+	});
+	sendNotes();
+	returnNotes();
+});
+
+returnNotes();
+$("#currentTime").text(currentTime);
